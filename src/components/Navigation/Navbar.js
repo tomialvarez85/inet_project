06fb3b0bc5c16063d43ec87778ef2b2a8b4b4812
tomialvarez85 <tomialvarez78@gmail.com>
2022-09-13 17:30:48 -0300

    Updating Navbar

--- src/components/Navigation/Navbar.js
+++ src/components/Navigation/Navbar.js
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import { logDOM } from '@testing-library/react';
 import "./Navbar.css"
-import Logo from "../../media/Logo.png";
 import { Link } from "react-router-dom";
 
 
