4ff6aac1bd5c5c688dd83bbd299b0f602e507c31
tomialvarez85 <tomialvarez78@gmail.com>
2022-09-15 09:06:50 -0300

    Fixing package.json

--- src/paginas/Exposiciones.js
+++ src/paginas/Exposiciones.js
@@ -1,7 +1,7 @@
-import {useState, useEffect} from 'react';
+import React, {useState, useEffect} from 'react';
 import Navigation from '../components/Navigation/Navbar';
 import './Exposiciones.css';
-import React from 'react'
+
 
 export default function Exposiciones() {
   return (
