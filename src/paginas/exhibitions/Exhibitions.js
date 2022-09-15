2c9ede9daf13feadc348e04cd2e2d758220213a6
Matias-C <55718713+Matias-C@users.noreply.github.com>
2022-09-15 13:01:17 -0300

    dasda

--- src/paginas/exhibitions/Exhibitions.js
+++ src/paginas/exhibitions/Exhibitions.js
@@ -2,11 +2,9 @@ import React from 'react'
 
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
 
 import PageHeader from '../../components/PageHeader';
 import ExhibitionCard from './ExhibitionCard';
-import Modal from '../../components/Modal';
 
 import './Exhibitions.css';
 
